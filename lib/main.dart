da699b22fb07d7361102a8966c7b2ca6c22cf0ff
pranavsakthivel <pransakthivel@gmail.com>
2019-08-15 20:00:18 -0700

    Added firestore cloud dependencies
    Possible crashes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -12,15 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Usage Analytics App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.green,
       ),
       home: MyHomePage(title: 'Usage Analytics Tracker'),
