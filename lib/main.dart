cb084a0609fd23ab88d31de67458816fa15e7993
pranavsakthivel <pransakthivel@gmail.com>
2019-08-21 21:13:21 -0700

    Updated Android Studio

--- lib/main.dart
+++ lib/main.dart
@@ -40,27 +40,14 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       drawer: new Drawer(
@@ -76,7 +63,25 @@ class _MyHomePageState extends State<MyHomePage> {
                 new Divider(),
                 new ListTile(
                   title: new Text('About'),
-                  onTap: () {},
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute<void>(
+                      builder: (BuildContext context) {
+                        return Scaffold(
+                          appBar: AppBar(title: Text('About')),
+                          body: Center(
+
+                            child: RaisedButton(
+                              child: Text('Home'),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            ),
+                          ),
+
+                        );
+                      },
+                    ));
+                  },
                 ),
                 new Divider(),
               ]
